d8e0b3271d4341367898220bc85d9ede32b67e42
shrey183 <shrey183@gmail.com>
2018-09-21 17:57:25 +0200

    Update Attempt_1.js

--- Attempt_1.js
+++ Attempt_1.js
@@ -15,93 +15,80 @@ var assigned = [];
 // Buffer to hold tasks in the case when they cannot be immediately assigned
 var buffer = [];
 
-var tasks_completed = 0;
+var count = 0;
+var ic = 0;
 
 function request_location(assigned) {
+    var assigned_copy = assigned; 
 
-    for (var i = 0; i < assigned.length; i++) {
+    for (var i = 0; i < assigned_copy.length; i++) {
 
-        var p_object = assigned[i];
+        var p_object = assigned_copy[i];
         var p_id = Object.keys(p_object)[0];
-        var theta = Math.atan2(p_object[p_id].x, p_object[p_id].y);
-        var dist = p_object[p_id].d;
-        
-       // pi is on his way to the location
-        if (p_object[p_id].leave <= cur_time && cur_time <= p_object[p_id].start) {
-            var t_delta = cur_time - p_object[p_id].leave;
-            people[p_id].x = 15 * (t_delta) * Math.cos(theta);
-            people[p_id].y = 15 * (t_delta) * Math.sin(theta);
-        }
 
-        // pi is at the location
-        else if (p_object[p_id].start <= cur_time && cur_time <= p_object[p_id].finish) {
+        if (cur_time <= p_object[p_id].leave && p_object[p_id].leave < cur_time + 5) {
+            // send p_id to the destination
             people[p_id].x = p_object[p_id].x;
             people[p_id].y = p_object[p_id].y;
+            ic = ic + 1;
+        }
+        else if (cur_time-5 <= p_object[p_id].finish && p_object[p_id].finish < cur_time) {
+            // send p_id to the origin  
+            people[p_id].x = 0;
+            people[p_id].y = 0;
+            // Make p_id available
+            available.push(p_id);
+            // Unassign p_id
+            assigned.splice(p_object, 1);
+            count = count + 1;
         }
 
-        // pi is returning from the location to the origin 
-        else if (p_object[p_id].finish < cur_time && cur_time <= p_object[p_id].return_time) {
-            // pi has finished the task and so it must be removed from assigned
-            assigned.splice(assigned.indexOf(p_object), 1);
-            var t_delta = cur_time - p_object[p_id].finish;
-            people[p_id].x = (dist - 15 * (t_delta)) * Math.cos(theta);
-            people[p_id].y = (dist - 15 * (t_delta)) * (t_delta) * Math.sin(theta);
-            tasks_completed += 1;   
-        }
-        
-        else if (cur_time > p_object[p_id].return_time) {
-            // pi has returned to the origin. 
-            available.push(p_id);
-            people[p_id].x = 0;
-            people[p_id].y = 0;   
-        }
     }
     return people;
 }
 
 function attributeMissions(tasks) {
     buffer = buffer.concat(tasks);
-    
+
     if (0 < available.length && available.length < buffer.length) {
         // Then there aren't enough people available to assign to all tasks.
         for (var i = 0; i < available.length; i++) {
             var person_index = available[i];
-            var task = buffer.pop();
+            var task = buffer.shift();
             var dist = Math.pow(Math.pow(task.x, 2) + Math.pow(task.y, 2), 0.5);
             var obj = {};
             obj[person_index] = {
                 leave: task.start - (dist / 15) * 60,
-                return_time: task.start + task.length + (dist / 15) * 60,
-                start: task.start,
                 finish: task.start + task.length,
-                d: dist,
                 x: task.x,
                 y: task.y
             };
             assigned.push(obj);
         }
+        // Everyone has been assigned so no one is available. 
+        available = [];
     }
-    else if (available.length >= buffer.length){
+    else if (available.length >= buffer.length) {
         // There are enough people to assign to all tasks. 
-        for (var i = 0; i < tasks.length; i++) {
+        for (var i = 0; i < buffer.length; i++) {
             var person_index = available.shift();
             var dist = Math.pow(Math.pow(tasks[i].x, 2) + Math.pow(tasks[i].y, 2), 0.5);
             var obj = {};
             obj[person_index] = {
                 // The person should leave the origin at this time in order to reach at the destination on time. 
-                leave: tasks[i].start - (dist / 15) * 60, 
-                return_time: tasks[i].start + tasks[i].length + (dist / 15) * 60,
-                start: tasks[i].start,
+                leave: tasks[i].start - (dist / 15) * 60,
                 finish: tasks[i].start + tasks[i].length,
-                d: dist,
                 x: tasks[i].x,
                 y: tasks[i].y
             };
             assigned.push(obj);
         }
+        // All tasks in buffer have been assigned
+        buffer = [];
     }
     request_location(assigned);
     cur_time += 5;
-    console.log("Tasks Completed:" + tasks_completed);
+    console.log("Initiation Count " + ic);
+    console.log("Termination Count " + count);
     return people;
 }
